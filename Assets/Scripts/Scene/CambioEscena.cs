1b6ed04f0e004d2d9ee8a23ffd54980a94f481fc
Braian Arguello <braian.arguello@hotmail.com>
2025-07-02 13:03:08 -0300

    [ADD se agrega presentacion en otra escensa y mejora UI en habitacion ]

---
+++ Assets/Scripts/Scene/CambioEscena.cs
@@ -0,0 +1,60 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+////libreria sobre cambios de escena
+//using UnityEngine.SceneManagement;
+//public class MenuFinal : MonoBehaviour
+//{
+//    [SerializeField] private GameObject _creditsObject;
+//    public GameObject _menuObject;
+//    [SerializeField] private GameObject[] imagess;
+//    int _random;
+//    private void Start()
+//    {
+//        //_random = Random.Range(0, imagess.Length);
+//    }
+//    public void Jugar()
+//    {
+//        SceneManager.LoadScene(1);
+//    }
+//    public void Salir()
+//    {
+//        //codigo para cerrar la aplicacion
+//        Application.Quit();
+//    }
+//    public void ActivarOptions()
+//    {
+//        _menuObject.SetActive(false);
+//        _creditsObject.SetActive(true);
+//    }
+//    public void DesactivarOptions()
+//    {
+//        _creditsObject.SetActive(false);
+//        _menuObject.SetActive(true);
+//    }
+//    //public void metodoImagnes()
+//    //{
+//    // // Primero desactivamos todos los objetos del array
+//    // foreach (var item in imagess)
+//    // {
+//    // item.SetActive(false);
+//    // }
+//    // // Elegimos uno aleatoriamente
+//    // int randomIndex = Random.Range(0, imagess.Length);
+//    // // Activamos solo el objeto aleatorio
+//    // imagess[randomIndex].SetActive(true);
+//    //}
+//}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Necesario para gestionar escenas
+
+public class CambioEscena : MonoBehaviour
+{
+    // Función pública que se llamará desde el botón
+    public void CargarNuevaEscena()
+    {
+        SceneManager.LoadScene(1); // Usa el índice que anotaste de la escena de la casa
+    }
+}
